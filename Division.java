c16125b5fdf21def252669e91967b9925b7ec8cd
Harika Deepthi Govvala <147792047+23P31A0518@users.noreply.github.com>
2024-09-20 23:55:34 +0530

    import java.util.Scanner; public class divisiom {     public static void main(String[]  args)     {         int a,b;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         b=sc.nextInt();         int div=a/b;         System.out.printf("%d",div);         } }

---
+++ Division.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class divisiom
+{
+    public static void main(String[]  args)
+    {
+        int a,b;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        int div=a/b;
+        System.out.printf("%d",div);
+        }
+}
